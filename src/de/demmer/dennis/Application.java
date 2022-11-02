90e329cf471494d63ee02e31cd36d1496a8372cb
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-11-02 19:16:42 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,6 +1,62 @@
 package de.demmer.dennis;
 
+import java.util.Scanner;
+
 public class Application {
+	
+	public static void main(String[] args) {
+		
+		int i = isPositive(0);
+		System.out.println("Ergebnis ist: " + i);
+	
+		Scanner sc = new Scanner(System.in);
+		userInput(sc.nextLine());
+		
+	}
+	
+	
+	static int isPositive(int zahl) {
+		if(zahl > 0) {
+			return 1;
+		} else if (zahl < 0) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	
+
+	static void userInput(String input) {
+		
+		// "switch" + STRG + LEER -> "switch-case statement" wählen
+		
+		switch(input) {
+		
+		case "exit":
+			System.out.println("Das Programm wird beendet");
+			break;
+		case "pause":
+			System.out.println("Das Programm wird pausiert");
+			break;
+		case "start":
+			System.out.println("Das Programm wird gestartet");
+			break;
+		case "hilfe":
+			System.out.println("exit - beendet das Programm");
+			System.out.println("start - startet das Programm");
+			System.out.println("pause - pausiert das Programm");
+			break;
+		default:
+			System.out.println("Eingabe ungültig. Tippe 'hilfe' ein um alle Befehle aufgelistet zu bekommen");
+			break;
+		}
+		
+	}
+	
+	
+	
+	
+	
 	
 
 }
